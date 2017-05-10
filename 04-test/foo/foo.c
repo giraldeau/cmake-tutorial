f6eb45622d7a564425e38656e6806afa4f098e00
Francis Giraldeau <francis.giraldeau@gmail.com>
2017-05-10 14:50:38 -0400

    Add ctest example
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>

---
+++ 04-test/foo/foo.c
@@ -0,0 +1,6 @@
+#include <stdlib.h>
+#include <stdio.h>
+void foo()
+{
+	printf("foo1\n");
+}
---
+++ 04-test/hello.c
@@ -0,0 +1,9 @@
+#include <stdlib.h> 
+#include <stdio.h> 
+#include "foo/foo.h"
+int main()
+{
+	printf("hello\n");
+	foo();
+	return 0;
+}
